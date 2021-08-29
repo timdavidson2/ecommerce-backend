3af32670196416e05bc9e29d5d9822642b401a96
Tim Davidson <timdavidson2@gmail.com>
2021-08-29 16:17:17 -0400

    Update index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,10 +1,14 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Categories have many Products
 
